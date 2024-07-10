31b17f801d247957a24778ca7049d463017c1a6c
Pamella Siqueira <contatopamellasiqueira@gmail.com>
2024-07-10 16:47:39 -0300

    responsive for smartphone

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-
-// alert("Buenas");
+function toggleMenu() {
+    var menu = document.getElementById('navResponsive');
+    var hamburguer = document.getElementById('menuHamburguer');
+    menu.classList.toggle('open');
+    hamburguer.classList.toggle('change');
+}
